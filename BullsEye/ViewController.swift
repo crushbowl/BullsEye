553daaa661fc17e97a4cd71befbf00bf77aa4098
JanL <janelle.h.writes@gmail.com>
2016-08-13 17:44:01 -0400

    Add game logic

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,22 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentValue: Int = 0
+    
+    var currentValue: Int = 50
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+        updateLabels()
     }
 
 
     @IBAction func showAlert(sender: UIButton) {
-        let alert = UIAlertController(title: "Hello World", message: "This is my 100th app", preferredStyle: .Alert)
         
-        let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
+        let difference: Int
+        if currentValue > targetValue {
+            difference = currentValue - targetValue
+        } else if targetValue > currentValue {
+            difference = targetValue - currentValue
+        } else {
+            difference = 0
+        }
+        
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)" + "\nThe difference is: \(difference)"
+        
+        
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
         
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        updateLabels()
         
     }
 
@@ -33,5 +56,19 @@ class ViewController: UIViewController {
         currentValue = lroundf(slider.value)
     }
 
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        //currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        // alternative way to code the above
+        // targetLabel.text = "\(targetValue)"
+    }
+    
+    
 }
 
